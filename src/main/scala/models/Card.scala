71a89eda39999eabe368028c990bdf0380c4b745
Aaditya Joshua Srivastava <16014508@imail.sunway.edu.my>
2024-12-30 14:44:53 +0800

    moving files

---
+++ src/main/scala/models/Card.scala
@@ -0,0 +1,7 @@
+package models
+
+abstract class Card(val name: String, val description: String){
+  def cardType: String
+  def use(user: GameEntity, target: GameEntity): Unit
+  override def toString: String = s"$cardType: $name - $description"
+}
